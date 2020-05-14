dd0091f4edbffa2d05e03fa8fbf8e965139de4f5
Rafael Menon <rafael13rodrigo@gmail.com>
2020-05-14 17:54:03 -0300

    fist commit.

---
+++ src/users/users.controller.ts
@@ -0,0 +1,37 @@
+import { Controller, Post, Body, UseGuards, Get, Param } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
+import { ReturnUserDto } from './dto/return-user.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { RolesGuard } from '../auth/roles.guard';
+import { Role } from '../auth/role.decorator';
+import { UserRole } from './user-roles.enum';
+
+@Controller('users')
+@UseGuards(AuthGuard(), RolesGuard)
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Post()
+  @Role(UserRole.ADMIN)
+  @UseGuards(AuthGuard(), RolesGuard)
+  async createAdminUser(
+    @Body() createUserDto: CreateUserDto,
+  ): Promise<ReturnUserDto> {
+    const user = await this.usersService.createAdminUser(createUserDto);
+    return {
+      user,
+      message: 'Adminsitrador cadastrado com sucesso',
+    };
+  }
+
+  @Get(':id')
+  @Role(UserRole.ADMIN)
+  async findUserById(@Param('id') id): Promise<ReturnUserDto> {
+    const user = await this.usersService.findUserById(id);
+    return {
+      user,
+      message: 'Usuário encontrado',
+    };
+  }
+}
